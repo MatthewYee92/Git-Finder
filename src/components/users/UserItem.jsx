13ed01cc1cedaa0dc04c52e20e115638c67d15fd
Matthew Yee <matthewyee@Matthews-MacBook-Pro.local>
2019-07-12 15:20:50 -0700

    refactored UserItem a little

--- src/components/users/UserItem.jsx
+++ src/components/users/UserItem.jsx
@@ -1,23 +1,23 @@
 import React, { Component } from 'react'
 
 export class UserItem extends Component {
-    constructor() {
-        super()
-        this.state = {
+
+        state = {
           id: 'id',
           login: 'matthewyee92',
           avatar_url: 'https://avatars2.githubusercontent.com/u/32374888?v=4',
           html_url: 'https://github.com/MatthewYee92'
         }
-    }
+    
     
     render() {
+        const { login, avatar_url, html_url } = this.state
         return (
             <div className='card text-center'>
-              <img src={this.state.avatar_url} alt="" className='round-img' style={{ width: '60px' }}/>
-              <h3>{this.state.login}</h3>
+              <img src={avatar_url} alt="" className='round-img' style={{ width: '60px' }}/>
+              <h3>{login}</h3>
               <div>
-                  <a href={this.state.html_url} className="btn btn-dark btn-sm my-1">More</a>
+                  <a href={html_url} className="btn btn-dark btn-sm my-1">More</a>
               </div>
             </div>
         )
