1c755efb4b53d7317de821076ba48443c0638cbc
Matthew Yee <matthewyee@Matthews-MacBook-Pro.local>
2019-07-12 16:35:15 -0700

    added spinner gif while page loads

--- src/components/users/Users.jsx
+++ src/components/users/Users.jsx
@@ -1,14 +1,17 @@
 import React, { Component } from "react";
 import UserItem from "./UserItem";
+import Spinner from '../layout/Spinner'
 
-export class Users extends Component {
-  render() {
-    return (
-      <div style={userStyle}>
-        {this.props.users.map(user => (
-          <UserItem key={user.id} user={user} />
-        ))}
-      </div>
+const Users = ({ users, loading }) => {
+    if(loading) {
+        return <Spinner />
+    } else {
+        return (
+            <div style={userStyle}>
+              {users.map(user => (
+                <UserItem key={user.id} user={user} />
+              ))}
+            </div>
     );
   }
 }
