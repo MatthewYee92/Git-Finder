8f6b5555e68eac383162181303971ce2639a11df
Matthew Yee <matthewyee@Matthews-MacBook-Pro.local>
2019-07-13 02:42:13 -0700

    added react router minor implementation

---
+++ src/components/pages/About.jsx
@@ -0,0 +1,14 @@
+import React, { Fragment } from "react";
+
+const About = () => {
+  return (
+    <Fragment>
+      <h1>About This App</h1>
+      <p>App to ease the Search of Github Users</p>
+      <p>Made by: Matthew Yee</p>
+      <p>Version 1.0.0</p>
+    </Fragment>
+  );
+};
+
+export default About;
---
+++ src/components/users/User.jsx
@@ -0,0 +1,48 @@
+import React, { Component, Fragment } from "react";
+import Spinner from '../layout/Spinner'
+import PropTypes from 'prop-types'
+import { Link } from 'react-router-dom'
+
+
+export class User extends Component {
+  componentDidMount() {
+    this.props.getUser(this.props.match.params.login)
+  }
+
+
+  static propTypes = {
+    loading: PropTypes.bool,
+    user: PropTypes.object.isRequired,
+    getUser: PropTypes.func.isRequired  
+  }
+
+
+  render() {
+    const {
+      name,
+      avatar_url,
+      location,
+      bio,
+      blog,
+      login,
+      html_url,
+      followers,
+      following,
+      public_repos,
+      public_gists,
+      hireable
+    } = this.props.user;
+
+    const { loading } = this.props;
+
+    if(loading) return <Spinner/>
+ 
+    return <Fragment>
+          <Link to='/' className='btn btn-light'>
+            Back To Search
+          </Link>
+         </Fragment>
+  }
+}
+
+export default User;
