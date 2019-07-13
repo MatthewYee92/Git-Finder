2da117c86766da5b1f5a23be41c9162c62237edb
Matthew Yee <matthewyee@Matthews-MacBook-Pro.local>
2019-07-13 03:36:34 -0700

    prep for deployment on github pages

---
+++ build/precache-manifest.e2c1bada81fdfb610091c07ba196b452.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b2839cc3bfbb4c944743f3efbd10587",
+    "url": "/index.html"
+  },
+  {
+    "revision": "753edc0d7b57c1e532d4",
+    "url": "/static/css/main.f65227a3.chunk.css"
+  },
+  {
+    "revision": "ddd346e0af0970134a27",
+    "url": "/static/js/2.e56676bb.chunk.js"
+  },
+  {
+    "revision": "753edc0d7b57c1e532d4",
+    "url": "/static/js/main.44823a13.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
