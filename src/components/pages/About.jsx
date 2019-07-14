085a4c22dcb3ac493dc7e5db1a782315acda68ea
Matthew Yee <matthewyee@Matthews-MacBook-Pro.local>
2019-07-14 12:54:42 -0700

    updated about section

--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -4,7 +4,7 @@ const About = () => {
   return (
     <Fragment>
       <h1>About This App</h1>
-      <p>App to ease the Search of Github Users</p>
+      <p>App to ease the search of hiring software engineers</p>
       <p>Made by: Matthew Yee</p>
       <p>Version 1.0.0</p>
     </Fragment>
