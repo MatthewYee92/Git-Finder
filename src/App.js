cba550e508f2c45edea2501ec1a212813c407a12
Matthew Yee <matthewyee@Matthews-MacBook-Pro.local>
2019-07-14 12:09:44 -0700

    converted search class comp to function using react hooks

--- src/App.js
+++ src/App.js
@@ -32,8 +32,8 @@ class App extends Component {
     this.setState({ loading: true });
 
     const res = await axios.get(`https://api.github.com/users/${username}?client_id=
-            ${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=
-            ${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+        ${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=
+        ${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
 
     this.setState({ user: res.data, loading: false });
     console.log("getUser");
